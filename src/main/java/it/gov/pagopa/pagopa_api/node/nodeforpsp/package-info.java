63a50e0dd858961a2af60c7d5b1e355e0c328b95
d.sabatini <d.sabatini@accenture.com>
2023-02-06 18:55:56 +0100

    Removed generated soap classes from src folder

--- src/main/java/it/gov/pagopa/pagopa_api/node/nodeforpsp/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://pagopa-api.pagopa.gov.it/node/nodeForPsp.xsd")
-package it.gov.pagopa.pagopa_api.node.nodeforpsp;
--- src/main/java/it/gov/pagopa/pagopa_api/xsd/common_types/v1_0/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://pagopa-api.pagopa.gov.it/xsd/common-types/v1.0.0/")
-package it.gov.pagopa.pagopa_api.xsd.common_types.v1_0;
