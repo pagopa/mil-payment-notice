c41b41736abd88d2fd85e7f17aa796506cc4f6b2
riccardo.giuliani <riccardo.giuliani@nttdata.com>
2023-10-17 15:45:31 +0200

    Modified rest resource

---
+++ src/main/java/it/pagopa/swclient/mil/paymentnotice/client/MilRestResource.java
@@ -0,0 +1,25 @@
+package it.pagopa.swclient.mil.paymentnotice.client;
+
+import io.smallrye.mutiny.Uni;
+import it.pagopa.swclient.mil.paymentnotice.client.bean.AcquirerConfiguration;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+
+/**
+ * Reactive rest client for the REST APIs exposed by the MIL APIM
+ */
+@RegisterRestClient(configKey = "mil-rest-api")
+public interface MilRestResource {
+
+	/**
+	 * Retrieves the psp configuration
+	 * @param acquirerId the acquirer id passed in request
+	 * @return the psp configuration for the acquirer id
+	 */
+	@GET
+	@Path("/acquirers/{acquirerId}.json")
+    Uni<AcquirerConfiguration> getPspConfiguration(@PathParam(value = "acquirerId") String acquirerId);
+
+}
