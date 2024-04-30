d37024e2dbca87f37eefe349b16088316eb61b1f
antoniotarricone <antonio.tarricone@pagopa.it>
2024-04-30 17:40:34 +0200

    Used mil-common to trace (Otel) MongoDB accesses.

--- src/main/java/it/pagopa/swclient/mil/paymentnotice/dao/PaymentTransactionRepository.java
+++ src/main/java/it/pagopa/swclient/mil/paymentnotice/dao/PaymentTransactionRepository.java
@@ -3,12 +3,12 @@ package it.pagopa.swclient.mil.paymentnotice.dao;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepositoryBase;
-import it.pagopa.swclient.mil.paymentnotice.utils.Trace;
+import it.pagopa.swclient.mil.observability.TraceReactivePanacheMongoRepository;
 
 /**
  *  MongoDB repository for payment transactions, reactive flavor
  */
-@Trace
+@TraceReactivePanacheMongoRepository
 @ApplicationScoped
 public class PaymentTransactionRepository implements ReactivePanacheMongoRepositoryBase<PaymentTransactionEntity, String> { //NOSONAR
 	
